0322e5016ebc4ede9171a11dfd5087f51292045e
Anurag Vaidya <ajv012@bucknell.edu>
2020-11-04 09:07:13 -0500

    Created response (empty currently)

---
+++ src/main/java/Response.java
@@ -0,0 +1,21 @@
+/* *****************************************
+ * CSCI205 - Software Engineering and Design
+ * Fall 2020
+ * Instructor: Prof. Brian King
+ *
+ * Name: Anurag Vaidya
+ * Section: 8:50 AM
+ * Date: 11/4/2020
+ * Time: 9:06 AM
+ *
+ * Project: csci205FinalProject
+ * Package: PACKAGE_NAME
+ * Class: Response
+ *
+ * Description:
+ *
+ * ****************************************
+ */
+
+public class Response {
+}
